35f3ac4166f5e34e934a33186d047b03a3e8e88e
Darius Wattimena <dariuswattimena@hotmail.com>
2022-03-16 19:41:18 +0100

    Added support for Discord webhooks when certain events happen (e.g. user is created)

---
+++ src/main/kotlin/nl/greaper/bnplanner/client/DiscordWebhookClient.kt
@@ -0,0 +1,65 @@
+package nl.greaper.bnplanner.client
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import nl.greaper.bnplanner.config.DiscordConfig
+import nl.greaper.bnplanner.model.discord.EmbedColor
+import nl.greaper.bnplanner.model.discord.EmbedFooter
+import nl.greaper.bnplanner.model.discord.EmbedMessage
+import nl.greaper.bnplanner.model.discord.EmbedThumbnail
+import nl.greaper.bnplanner.model.discord.Message
+import nl.greaper.bnplanner.model.discord.getValue
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+import java.time.Instant
+
+@Component
+class DiscordWebhookClient(
+    private val config: DiscordConfig,
+    private val objectMapper: ObjectMapper
+) {
+
+    private val rest = RestTemplate()
+    private val headers = HttpHeaders()
+
+    fun send(
+        description: String,
+        color: EmbedColor,
+        thumbnail: EmbedThumbnail,
+        footer: EmbedFooter,
+        confidential: Boolean = true
+    ) {
+        return send(
+            EmbedMessage(
+                description = description,
+                timestamp = Instant.now().toString(),
+                color = color.getValue(),
+                thumbnail = thumbnail,
+                footer = footer
+            ),
+            confidential
+        )
+    }
+
+    private fun send(embedMessage: EmbedMessage, confidential: Boolean) {
+        headers.contentType = MediaType.APPLICATION_JSON
+        // Private discord server with all messages
+        val webhookUrl = config.webhook
+
+        val body = objectMapper.writeValueAsString(Message(listOf(embedMessage)))
+        val request = HttpEntity(body, headers)
+
+        if (!confidential) {
+            // Public catch mapping hub feed with only the most informative messages
+            val moddingServerWebhookUrl = config.webhookPublic
+            rest.exchange(moddingServerWebhookUrl, HttpMethod.POST, request, String::class.java)
+        }
+
+        if (webhookUrl.isNotBlank()) {
+            rest.exchange(webhookUrl, HttpMethod.POST, request, String::class.java)
+        }
+    }
+}
---
+++ src/main/kotlin/nl/greaper/bnplanner/model/discord/EmbedMessage.kt
@@ -0,0 +1,87 @@
+package nl.greaper.bnplanner.model.discord
+
+import nl.greaper.bnplanner.model.discord.EmbedColor.AQUA
+import nl.greaper.bnplanner.model.discord.EmbedColor.BLUE
+import nl.greaper.bnplanner.model.discord.EmbedColor.DARKER_GREY
+import nl.greaper.bnplanner.model.discord.EmbedColor.DARK_AQUA
+import nl.greaper.bnplanner.model.discord.EmbedColor.DARK_BLUE
+import nl.greaper.bnplanner.model.discord.EmbedColor.DARK_GOLD
+import nl.greaper.bnplanner.model.discord.EmbedColor.DARK_GREEN
+import nl.greaper.bnplanner.model.discord.EmbedColor.DARK_GREY
+import nl.greaper.bnplanner.model.discord.EmbedColor.DARK_NAVY
+import nl.greaper.bnplanner.model.discord.EmbedColor.DARK_ORANGE
+import nl.greaper.bnplanner.model.discord.EmbedColor.DARK_PURPLE
+import nl.greaper.bnplanner.model.discord.EmbedColor.DARK_RED
+import nl.greaper.bnplanner.model.discord.EmbedColor.DARK_VIVID_PINK
+import nl.greaper.bnplanner.model.discord.EmbedColor.GOLD
+import nl.greaper.bnplanner.model.discord.EmbedColor.GREEN
+import nl.greaper.bnplanner.model.discord.EmbedColor.GREY
+import nl.greaper.bnplanner.model.discord.EmbedColor.LIGHT_GREY
+import nl.greaper.bnplanner.model.discord.EmbedColor.LUMINOUS_VIVID_PINK
+import nl.greaper.bnplanner.model.discord.EmbedColor.NAVY
+import nl.greaper.bnplanner.model.discord.EmbedColor.ORANGE
+import nl.greaper.bnplanner.model.discord.EmbedColor.PURPLE
+import nl.greaper.bnplanner.model.discord.EmbedColor.RED
+
+data class EmbedMessage(
+    val description: String,
+    val timestamp: String,
+    val color: Int,
+    val thumbnail: EmbedThumbnail,
+    val footer: EmbedFooter
+)
+
+data class EmbedThumbnail(val url: String)
+data class EmbedFooter(val text: String, val icon_url: String? = null)
+
+enum class EmbedColor {
+    AQUA,
+    GREEN,
+    BLUE,
+    PURPLE,
+    GOLD,
+    ORANGE,
+    RED,
+    GREY,
+    DARKER_GREY,
+    NAVY,
+    DARK_AQUA,
+    DARK_GREEN,
+    DARK_BLUE,
+    DARK_PURPLE,
+    DARK_GOLD,
+    DARK_ORANGE,
+    DARK_RED,
+    DARK_GREY,
+    LIGHT_GREY,
+    DARK_NAVY,
+    LUMINOUS_VIVID_PINK,
+    DARK_VIVID_PINK
+}
+
+fun EmbedColor.getValue(): Int {
+    return when (this) {
+        AQUA -> 1752220
+        GREEN -> 3066993
+        BLUE -> 3447003
+        PURPLE -> 10181046
+        GOLD -> 15844367
+        ORANGE -> 15105570
+        RED -> 15158332
+        GREY -> 9807270
+        DARKER_GREY -> 8359053
+        NAVY -> 3426654
+        DARK_AQUA -> 1146986
+        DARK_GREEN -> 2067276
+        DARK_BLUE -> 2123412
+        DARK_PURPLE -> 7419530
+        DARK_GOLD -> 12745742
+        DARK_ORANGE -> 11027200
+        DARK_RED -> 10038562
+        DARK_GREY -> 9936031
+        LIGHT_GREY -> 12370112
+        DARK_NAVY -> 2899536
+        LUMINOUS_VIVID_PINK -> 16580705
+        DARK_VIVID_PINK -> 12320855
+    }
+}
